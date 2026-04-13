00c298f5bcbb08cb0bd54627330d1a8883045444
grospitz-wreck-it <grospitz@gmail.com>
2026-04-13 19:31:21 +0200

    Refactor supabase import to use createClient

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 import { SUPABASE_URL, SUPABASE_KEY } from "./config.js";
-import { supabase } from "./client.js";
+
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // =====================
