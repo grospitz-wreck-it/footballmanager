ee906988cce54663b56452a8abaeff2abb70573e
grospitz-wreck-it <grospitz@gmail.com>
2026-04-13 07:39:15 +0200

    Update ui.js

--- js/ui/ui.js
+++ js/ui/ui.js
@@ -177,14 +177,7 @@ container.appendChild(div);
 // =========================
 // 🎮 OVERLAY TRIGGER
 // =========================
-if(newest.assets?.length){
 
-  const img = newest.assets[0]?.url;
-
-  if(img){
-    showOverlay(img, text);
-  }
-}
 }
 const overlayEl = document.getElementById("matchOverlay");
 const overlayImg = document.getElementById("overlayImage");
