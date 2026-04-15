aad689f02599a3027a0d247f24b4d04e2f6067d4
grospitz-wreck-it <grospitz@gmail.com>
2026-04-15 21:26:57 +0200

    Update teamGenerator.js

--- js/modules/teamGenerator.js
+++ js/modules/teamGenerator.js
@@ -67,8 +67,7 @@ async function fetchPlayers(pos, n){
   const { data, error } = await supabase
     .from("players")
     .select("id, position, position_type")
-    .is("team_id", null)
-    .or(filter)
+    .or(`${filter},team_id.is.null`)
     .limit(n * 4); // 🔥 Puffer
 
   if(error){
