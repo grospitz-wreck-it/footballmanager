73878ecfe6aa59f682d3b206e5c68ded2691aea1
grospitz-wreck-it <grospitz@gmail.com>
2026-04-17 22:52:32 +0200

    Update client.js

--- js/client.js
+++ js/client.js
@@ -2,6 +2,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 import { SUPABASE_URL, SUPABASE_KEY } from './config.js';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
+console.log("URL:", SUPABASE_URL);
+console.log("KEY:", SUPABASE_KEY);
+console.log("CLIENT:", supabase);
 console.log("🔥 Supabase Client erstellt");
 
