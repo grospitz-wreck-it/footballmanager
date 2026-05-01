2089654ea8463d9c22ba522b6b6c2c06018e0cdd
grospitz-wreck-it <grospitz@gmail.com>
2026-05-01 20:36:01 +0200

    Create penaltyUI.js

---
+++ content/minigames/penalty/penaltyUI.js
@@ -0,0 +1,12 @@
+<div class="penalty-game" data-penalty-root>
+  <div class="penalty-hud">
+    <span data-penalty-round>Round 1/5</span>
+    <span data-penalty-score>0 : 0</span>
+    <span data-penalty-feedback>Tap or swipe to shoot</span>
+  </div>
+  <div class="penalty-pitch" data-penalty-pitch>
+    <div class="penalty-goal"></div>
+    <div class="penalty-keeper" data-penalty-keeper data-direction="center"></div>
+    <div class="penalty-ball" data-penalty-ball></div>
+  </div>
+</div>
