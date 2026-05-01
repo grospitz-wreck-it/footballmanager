1c62dc8a995cee3320430c4086a56c4c43012350
grospitz-wreck-it <grospitz@gmail.com>
2026-05-01 22:14:02 +0200

    Add penaltySprites.js with asset definitions

---
+++ content/minigames/penalty/penaltySprites.js
@@ -0,0 +1,41 @@
+export const PENALTY_ASSETS = {
+  keeper: {
+    idle: './assets/keeper/keeper_idle_center.png',
+    bounce: './assets/keeper/keeper_idle_bounce.png',
+    diveLeft1: './assets/keeper/keeper_dive_left_1.png',
+    diveLeft2: './assets/keeper/keeper_dive_left_2.png',
+    saveLeft: './assets/keeper/keeper_save_left.png',
+    miss: './assets/keeper/keeper_miss.png',
+    shadow: './assets/keeper/keeper_shadow.png'
+  },
+
+  ball: {
+    default: './assets/ball/ball_default.png',
+    spin1: './assets/ball/ball_spin_1.png',
+    spin2: './assets/ball/ball_spin_2.png',
+    glow: './assets/ball/ball_glow.png'
+  },
+
+  stadium: {
+    crowd: './assets/stadium/crowd_bg.png',
+    lights: './assets/stadium/stadium_lights.png',
+    goal: './assets/stadium/goal_frame.png',
+    net: './assets/stadium/net_overlay.png',
+    scoreboard: './assets/stadium/scoreboard_frame.png'
+  },
+
+  ui: {
+    goalPopup: './assets/ui/goal_popup.png',
+    savePopup: './assets/ui/save_popup.png',
+    perfectPopup: './assets/ui/perfect_popup.png',
+    powerBar: './assets/ui/power_bar_frame.png'
+  },
+
+  fx: {
+    goalFlash: './assets/fx/flash_goal.png',
+    saveFlash: './assets/fx/flash_save.png',
+    particle: './assets/fx/particle_star.png',
+    neonBurst: './assets/fx/neon_burst.png',
+    crt: './assets/fx/crt_scanline.png'
+  }
+};
