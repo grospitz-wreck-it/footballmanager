24dffa746623143f11e563620234578ab8e40d2b
grospitz-wreck-it <grospitz@gmail.com>
2026-05-02 11:34:06 +0200

    Update penaltySprites.js

--- content/minigames/penalty/penaltySprites.js
+++ content/minigames/penalty/penaltySprites.js
@@ -1,9 +1,7 @@
 export const PENALTY_ASSETS = {
   keeper: {
-  idle: './assets/keeper/keeper_idle_center.png',
-  bounce: './assets/keeper/keeper_idle_bounce.png',
-  diveLeft1: './assets/keeper/keeper_dive_left_1.png'
-},
+  spriteSheet: './assets/keeper/keeper_sprite_sheet.webp'
+}
 ball: {
   default: './assets/ball/ball_default.webp'
 },
