420e20873bb15c6edf7e2575b1fdba493c7e3d57
grospitz-wreck-it <grospitz@gmail.com>
2026-05-02 12:41:00 +0200

    Fix formatting and structure of PENALTY_ASSETS

--- content/minigames/penalty/penaltySprites.js
+++ content/minigames/penalty/penaltySprites.js
@@ -1,12 +1,20 @@
 export const PENALTY_ASSETS = {
   keeper: {
-  spriteSheet: './assets/keeper/keeper_sprite_sheet.webp'
-}
-ball: {
-  default: './assets/ball/ball_default.webp'
-},
+    spriteSheet:
+      './assets/keeper/keeper_sprite_sheet.webp'
+  },
+
+  ball: {
+    default:
+      './assets/ball/ball_default.webp'
+  },
+
   stadium: {
-    background: './assets/stadium/stadium.webp'
-  }
-  
+    background:
+      './assets/stadium/stadium.webp'
+  },
+
+  ui: {},
+
+  fx: {}
 };
