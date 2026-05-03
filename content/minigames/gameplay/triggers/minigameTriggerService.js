31eb716e9e008f81a5e028a3f448654426750ead
grospitz-wreck-it <grospitz@gmail.com>
2026-05-03 15:34:41 +0200

    Create minigameTriggerService.js with trigger function
    Add stub for minigame trigger function.

---
+++ content/minigames/gameplay/triggers/minigameTriggerService.js
@@ -0,0 +1,4 @@
+/** Stub: spätere Übergänge zu Penalty/Freistoß/Keeper-Challenge. */
+export function triggerMinigame(type, payload) {
+  return { triggered: false, type, payload, note: 'Stub only in V1 sandbox.' };
+}
