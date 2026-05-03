d2d8aedadca634b56a5e8dd83b2c9e31e955a882
grospitz-wreck-it <grospitz@gmail.com>
2026-05-03 15:13:23 +0200

    Create gameplayConfig.js

---
+++ content/minigames/gameplay/config/gameplayConfig.js
@@ -0,0 +1,15 @@
+export const gameplayConfig = {
+  tickRate: 12,
+  matchStartMinute: 53,
+  canvasAspectRatio: 16 / 9,
+  team: {
+    home: { name: 'Home', color: '#00E5FF' },
+    away: { name: 'Away', color: '#FF4D8D' }
+  },
+  theme: {
+    bg: '#070B14',
+    pitch: '#127a3f',
+    line: '#9df7d1',
+    ball: '#F8F8FF'
+  }
+};
