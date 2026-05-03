6da6494aedb451ea92ebe7d00d46b38c1978dae6
grospitz-wreck-it <grospitz@gmail.com>
2026-05-03 15:19:23 +0200

    Add demoScenario for gameplay events

---
+++ content/minigames/gameplay/dev/demoScenario.js
@@ -0,0 +1,7 @@
+export const demoScenario = [
+  { minute: 53, second: 4, type: 'BALL_WIN', timelineText: '53’ Ballgewinn', ball: [0.28, 0.52], camera: [0.33, 0.5, 1] },
+  { minute: 54, second: 11, type: 'BUILDUP_RIGHT', timelineText: '54’ Aufbau rechts', ball: [0.44, 0.38], camera: [0.48, 0.42, 1.1] },
+  { minute: 55, second: 15, type: 'THROUGH_PASS', timelineText: '55’ Pass in die Spitze', ball: [0.66, 0.42], camera: [0.68, 0.44, 1.2] },
+  { minute: 56, second: 2, type: 'SHOT', timelineText: '56’ Abschluss', ball: [0.86, 0.5], camera: [0.82, 0.5, 1.35] },
+  { minute: 56, second: 7, type: 'GOAL', timelineText: '56’ TOR', ball: [0.97, 0.5], camera: [0.9, 0.5, 1.45] }
+];
