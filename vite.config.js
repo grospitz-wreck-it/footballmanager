b4f4a5726bfb2a0ff152088b3917dd8b6fd132ec
grospitz-wreck-it <grospitz@gmail.com>
2026-06-14 20:53:44 +0000

    „admin“

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "./",
+  base: "/",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
         admin: resolve(__dirname, "admin.html"),
+        adminLogin: resolve(__dirname, "admin-login.html"),
       },
     },
   },
