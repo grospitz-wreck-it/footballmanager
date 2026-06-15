3b8e6475e1f7dfe7afad9febfa08c3cd9b2d6f8a
grospitz-wreck-it <grospitz@gmail.com>
2026-06-15 17:47:11 +0000

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "/footballmanager/",
+
   build: {
     rollupOptions: {
       input: {
